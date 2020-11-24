231946deeca53c7838a4a8346542b4670d9b52b1
Mohamad Farizal Arifin <farizalarifin4777@gmail.com>
2020-11-24 19:45:24 +0700

    update python file

--- praktikum-mod4.py
+++ praktikum-mod4.py
@@ -6,12 +6,13 @@ print("Mata Kuliah : Bahasa Pemrograman")
 print("===================================================================")
 print("Tugas Praktikum module 4")
 
-# Buat program sederhana untuk menambahkan data kedalam sebuah list dengan rincian sebagai berikut:
-# • Progam meminta memasukkan data sebanyak-banyaknya (gunakan perulangan)
-# • Tampilkan pertanyaan untuk menambah data (y/t?), apabila jawaban t (Tidak), maka program akan menampilkan daftar datanya.
-# • Nilai Akhir diambil dari perhitungan 3 komponen nilai (tugas: 30%, uts: 35%, uas: 35%)
-# • Buat flowchart dan penjelasan programnya pada README.md.
-# • Commit dan push repository ke github.
+# Buat program sederhana untuk menambahkan data kedalam sebuah list dengan rincian sebagai berikut: • Progam meminta
+# memasukkan data sebanyak-banyaknya (gunakan perulangan) • Tampilkan pertanyaan untuk menambah data (y/t?),
+# apabila jawaban t (Tidak), maka program akan menampilkan daftar datanya. • Nilai Akhir diambil dari perhitungan 3
+# komponen nilai (tugas: 30%, uts: 35%, uas: 35%) • Buat flowchart dan penjelasan programnya pada README.md. • Commit
+# dan push repository ke github.
+
+from prettytable import PrettyTable
 
 baris = []
 stop = False
@@ -24,17 +25,20 @@ while (not stop):
     uts = input("Masukan Nilai UTS : ")
     uas = input("Masukan Nilai UAS : ")
     nilai_akhir = 0.3 * float(tugas) + 0.35 * float(uts) + 0.35 * float(uas)
-    baris.extend([nama, nim, tugas, uts, uas, nilai_akhir])
+    baris.append([nama, nim, tugas, uts, uas, nilai_akhir])
 
     tanya = input("Tambah data? (y/n) : ")
-    if(tanya == "n"):
+    if (tanya == "n"):
         stop = True
 
 # cetak nilai
 print("===================================================================")
 
-from prettytable import PrettyTable
 x = PrettyTable()
-x.field_names = ["N0","Nama","Nim","Tugas","UTS","UAS","Nilai Akhir"]
-x.add_row(["no",baris[0],baris[1],baris[2],baris[3],baris[4],baris[5]])
+no = 0
+
+for isi in baris:
+    no += 1
+    x.field_names = ["No", "Nama", "Nim", "Tugas", "UTS", "UAS", "Nilai Akhir"]
+    x.add_row([no, isi[0], isi[1], isi[2], isi[3], isi[4], isi[5]])
 print(x)
